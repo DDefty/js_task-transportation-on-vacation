3eb02e78a8084907c87462726105fa3c865a47f6
DDefty <dawid.kogut99@gmail.com>
2025-09-20 09:34:36 +0200

    Implement rental cost calculation logic in calculateRentalCost function

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,15 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const cost = 40;
+
+  if (days < 3) {
+    return cost * days;
+  } else if (days >= 3 && days < 7) {
+    return cost * days - 20;
+  } else {
+    return cost * days - 50;
+  }
 }
 
 module.exports = calculateRentalCost;
